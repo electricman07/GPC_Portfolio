ad2cdb846da2cc3819a5125e0e009073db7326be
Glen Popowich <glenp@telus.net>
2026-02-17 15:07:44 -0800

    added code for auto date in footer

---
+++ assets/js/scripts.js
@@ -0,0 +1,13 @@
+// Copyright date
+const getElement = (selector) => {
+  const element = document.querySelector(selector);
+
+  if (element) return element;
+  throw Error(
+    `Please double check your class names, there is no ${selector} class`,
+  );
+};
+
+const date = getElement("#date");
+const currentYear = new Date().getFullYear();
+date.textContent = currentYear;
